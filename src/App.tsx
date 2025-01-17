4dd0d9f6676ff2300c4b7bd5dfa8f91d88d9af1a
Richard Grover <38992201+richgrov@users.noreply.github.com>
2025-01-16 20:28:59 -0700

    Finish login/logout system

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
-import { useState, useContext } from "react";
+import { useState, useContext, useRef } from "react";
 import { AuthContext, pb } from "./pocketbase";
 import { Button } from "./components/ui/button";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "./components/ui/popover";
+import { Input } from "./components/ui/input";
 
 function AdminApp() {
   return (
@@ -10,8 +16,50 @@ function AdminApp() {
   );
 }
 
+function Login() {
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
+  async function onSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    try {
+      await pb
+        .collection("users")
+        .authWithPassword(emailRef.current!.value, passwordRef.current!.value);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  return (
+    <>
+      <form className="flex flex-col gap-3" onSubmit={onSubmit}>
+        <Input name="email" placeholder="Email" type="email" ref={emailRef} />
+        <Input
+          name="password"
+          placeholder="Password"
+          type="password"
+          ref={passwordRef}
+        />
+        <Button>Login</Button>
+      </form>
+    </>
+  );
+}
+
 function DefaultApp() {
-  return <p>Default App</p>;
+  return (
+    <>
+      <Popover>
+        <PopoverTrigger>
+          <Button>Admin Login</Button>
+        </PopoverTrigger>
+        <PopoverContent>
+          <Login />
+        </PopoverContent>
+      </Popover>
+    </>
+  );
 }
 
 function App() {
