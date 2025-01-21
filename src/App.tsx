889b634dd1cf48b28832823143494d25de36e64a
Christian Grey <christian@greyscale.space>
2025-01-20 18:23:40 -0700

    Revert changes to index.css; Use tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import { BrowserRouter, Route, Routes } from "react-router";
 
 import AdminApp from "@/pages/Admin";
 import IndexPage from "@/pages/Index";
+import Layout from "./pages/Layout";
 
 export default function App() {
   return <BrowserRouter>
     <Routes>
-      <Route path = "/" element={<IndexPage />} />
-      <Route path = "/admin" element={<AdminApp />} />
+      <Route element={<Layout />} >
+        <Route path = "/" element={<IndexPage />} />
+        <Route path = "/admin" element={<AdminApp />} />
+      </Route>
     </Routes>
   </BrowserRouter>;
 }
