c6baf682caf3848dc9c7b4dd1afcc9fdbda17002
Richard Grover <38992201+richgrov@users.noreply.github.com>
2025-01-21 11:20:51 -0700

    Add seat vision overlay

--- src/components/SeatDisplay.tsx
+++ src/components/SeatDisplay.tsx
@@ -1,6 +1,8 @@
 import { pocketBase } from "@/pocketbase";
 import { useEffect, useState } from "react";
 
+import vision from "@/assets/vision.webp";
+
 interface SeatRecord {
   DisplayName: string;
   X: number;
@@ -8,14 +10,26 @@ interface SeatRecord {
   Angle: number;
 }
 
+function toDegrees(angle: number) {
+  return (angle * 180) / Math.PI - 90;
+}
+
 function Seat(props: SeatRecord) {
   return (
-    <p
-      className="absolute bg-white p-3 rounded select-none"
-      style={{ top: props.Y * 2, left: props.X * 2 }}
-    >
-      {props.DisplayName}
-    </p>
+    <div className="absolute" style={{ top: props.Y * 2, left: props.X * 2 }}>
+      <img
+        className="absolute top-0 w-64 max-w-screen-2xl"
+        style={{
+          transform: `translate(-50%, -50%) rotate(${toDegrees(
+            props.Angle
+          )}deg)`,
+        }}
+        src={vision}
+      />
+      <p className="absolute top-0 -translate-x-1/2 -translate-y-1/2 bg-white bg-opacity-50 p-3 rounded select-none">
+        {props.DisplayName}
+      </p>
+    </div>
   );
 }
 
@@ -32,7 +46,7 @@ export function SeatDisplay() {
   }, []);
 
   return (
-    <div className="relative bg-white">
+    <div className="absolute bg-white">
       {seats.map((seat, i) => (
         <Seat key={i} {...seat} />
       ))}
