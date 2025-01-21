e289a9793a47262ceab82c85c1ab88bcc89c6b62
Christian Grey <christian@greyscale.space>
2025-01-20 18:01:01 -0700

    Added React Router; Moved admin stuff to /admin

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import IndexPage from "@/pages/Index";
 import Layout from "./pages/Layout";
 
 export default function App() {
-  return <BrowserRouter>
-    <Routes>
-      <Route element={<Layout />} >
-        <Route path = "/" element={<IndexPage />} />
-        <Route path = "/admin" element={<AdminApp />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<IndexPage />} />
+          <Route path="/admin" element={<AdminApp />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
