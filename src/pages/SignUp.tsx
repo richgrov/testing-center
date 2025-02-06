0b828b98c086d64359544f670f92ddd13e57a26a
Richard Grover <38992201+richgrov@users.noreply.github.com>
2025-02-06 12:52:29 -0700

    Add initial sign up table

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,3 +1,65 @@
+import {
+  Table,
+  TableCell,
+  TableHeader,
+  TableRow,
+  TableHead,
+} from "@/components/ui/table";
+import { parsePocketbaseDate, pocketBase } from "@/pocketbase";
+import { useEffect, useState } from "react";
+
+const enrollmentsCollection = pocketBase.collection("test_enrollments");
+
+interface Enrollment {
+  canvas_student_id: number;
+  canvas_student_name: string;
+  duration_mins: number;
+  link_sent: boolean;
+  start_test_at: string;
+  expand: {
+    test: {
+      course_code: string;
+      section: string;
+    };
+  };
+  unlock_after: string;
+}
+
 export function SignUpPage() {
-  return <p>Sign Up</p>;
+  const [enrollments, setEnrollments] = useState(new Array<Enrollment>());
+
+  useEffect(() => {
+    enrollmentsCollection
+      .getList(undefined, 500, { expand: "test" })
+      .then((data) => {
+        setEnrollments(data.items as any[] as Enrollment[]);
+      });
+  }, []);
+
+  enrollmentsCollection.subscribe("*", (data) => {
+    console.log(data);
+  });
+
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Name</TableHead>
+          <TableHead>Time</TableHead>
+          <TableHead>Course</TableHead>
+        </TableRow>
+      </TableHeader>
+      {enrollments.map((e) => (
+        <TableRow>
+          <TableCell>{e.canvas_student_name}</TableCell>
+          <TableCell>
+            {parsePocketbaseDate(e.start_test_at).toLocaleString()}
+          </TableCell>
+          <TableCell>
+            {e.expand.test.course_code + " " + e.expand.test.section}
+          </TableCell>
+        </TableRow>
+      ))}
+    </Table>
+  );
 }
--- src/pocketbase.ts
+++ src/pocketbase.ts
@@ -5,3 +5,7 @@ export const pocketBase = new PocketBase("http://localhost:8090");
 
 export interface Auth {}
 export const AuthContext = createContext<Auth | undefined>(undefined);
+
+export function parsePocketbaseDate(dateString: string): Date {
+  return new Date(dateString.replace(" ", "T"));
+}
