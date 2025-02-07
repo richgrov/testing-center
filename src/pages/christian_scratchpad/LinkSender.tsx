29f20de8a34eb06651b098e9d271eb20c3fa3551
Richard Grover <38992201+richgrov@users.noreply.github.com>
2025-02-07 08:42:01 -0700

    Code cleanup

--- src/pages/christian_scratchpad/LinkSender.tsx
+++ src/pages/christian_scratchpad/LinkSender.tsx
@@ -5,11 +5,13 @@ import { fetchForward } from "./utils";
 
 export default function LinkSender() {
   const [apiBase, setApiBase] = useState("https://lms.neumont.edu/api/v1/");
-  const [authHeader, setAuthHeader] = useState("")
+  const [authHeader, setAuthHeader] = useState("");
   const [testId, setTestId] = useState("");
   const [body, setBody] = useState("");
   const [subject, setSubject] = useState("");
-  const [linkBase, setLinkBase] = useState("https://testing-center.greyscale.space/test_slot/");
+  const [linkBase, setLinkBase] = useState(
+    "https://testing-center.greyscale.space/test_slot/"
+  );
 
   const [inProgress, setInProgress] = useState(false);
   async function execute() {
@@ -17,7 +19,9 @@ export default function LinkSender() {
     const coll = pocketBase.collection<TestEnrollment>("test_enrollments");
     try {
       const toSend = await coll.getFullList({
-        filter: pocketBase.filter("test = {:test} && link_sent = false", { test: testId })
+        filter: pocketBase.filter("test = {:test} && link_sent = false", {
+          test: testId,
+        }),
       });
       for (const enrollment of toSend) {
         const link = new URL(enrollment.id, linkBase).href;
@@ -26,22 +30,21 @@ export default function LinkSender() {
           method: "POST",
           url: new URL("conversations", apiBase).href,
           headers: {
-            "Authorization": authHeader,
+            Authorization: authHeader,
             "Content-Type": "application/json",
           },
           body: JSON.stringify({
             subject: subject,
             recipients: [enrollment.canvas_student_id.toString()],
-            body: body + "\n" + link
-          })
+            body: body + "\n" + link,
+          }),
         });
         console.log(conversation.status);
         if (conversation.status > 299) {
           console.warn(conversation);
           throw new Error("Died");
-        }
-        else await coll.update(enrollment.id, { link_sent: true });
-        await new Promise(resolve => setTimeout(resolve, 2000));
+        } else await coll.update(enrollment.id, { link_sent: true });
+        await new Promise((resolve) => setTimeout(resolve, 2000));
       }
     } catch (e) {
       console.error(e);
@@ -51,13 +54,59 @@ export default function LinkSender() {
     }
   }
 
-  return <div className="leading-normal">
-    <label>Canvas API base: <input className="border border-black" value={apiBase} onChange={e => setApiBase(e.target.value)} /></label><br />
-    <label>Canvas Authorization Header: <input className="border border-black" value={authHeader} onChange={e => setAuthHeader(e.target.value)}/></label><br />
-    <label>Pocketbase Test ID: <input className="border border-black" value={testId} onChange={e => setTestId(e.target.value)} /></label><br />
-    <label>Link Base: <input className="border border-black" value={linkBase} onChange={e => setLinkBase(e.target.value)} /></label><br />
-    <label>Subject: <input className="border border-black" value={subject} onChange={e => setSubject(e.target.value)} /></label><br />
-    <button onClick={execute}>Go!</button>
-    <textarea className="border border-black" value={body} onChange={e => setBody(e.target.value)}></textarea>
-  </div>
+  return (
+    <div className="leading-normal">
+      <label>
+        Canvas API base:{" "}
+        <input
+          className="border border-black"
+          value={apiBase}
+          onChange={(e) => setApiBase(e.target.value)}
+        />
+      </label>
+      <br />
+      <label>
+        Canvas Authorization Header:{" "}
+        <input
+          className="border border-black"
+          value={authHeader}
+          onChange={(e) => setAuthHeader(e.target.value)}
+        />
+      </label>
+      <br />
+      <label>
+        Pocketbase Test ID:{" "}
+        <input
+          className="border border-black"
+          value={testId}
+          onChange={(e) => setTestId(e.target.value)}
+        />
+      </label>
+      <br />
+      <label>
+        Link Base:{" "}
+        <input
+          className="border border-black"
+          value={linkBase}
+          onChange={(e) => setLinkBase(e.target.value)}
+        />
+      </label>
+      <br />
+      <label>
+        Subject:{" "}
+        <input
+          className="border border-black"
+          value={subject}
+          onChange={(e) => setSubject(e.target.value)}
+        />
+      </label>
+      <br />
+      <button onClick={execute}>Go!</button>
+      <textarea
+        className="border border-black"
+        value={body}
+        onChange={(e) => setBody(e.target.value)}
+      ></textarea>
+    </div>
+  );
 }
