5a7b9bf674afd8065b200582e4571c0083e09873
floralhaven <141456665+floralhaven@users.noreply.github.com>
2025-02-24 13:33:17 -0700

    Testing for Erin

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -11,8 +11,6 @@ import { useContext, useEffect, useState } from "react";
 import * as React from "react";
 import { format } from "date-fns";
 import { CalendarIcon } from "lucide-react";
-
-import { cn } from "@/lib/utils";
 import { Calendar } from "@/components/ui/calendar";
 import {
   Popover,
