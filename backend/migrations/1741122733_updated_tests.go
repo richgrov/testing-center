1edfb5afe133991e4b57c789cb86ef31a8a74354
floralhaven <141456665+floralhaven@users.noreply.github.com>
2025-03-04 14:12:47 -0700

    Remove current_enrollments and added max_enrollments to my pocketbase

---
+++ backend/migrations/1741122733_updated_tests.go
@@ -0,0 +1,43 @@
+package migrations
+
+import (
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_3643163317")
+		if err != nil {
+			return err
+		}
+
+		// remove field
+		collection.Fields.RemoveById("number1913416427")
+
+		return app.Save(collection)
+	}, func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_3643163317")
+		if err != nil {
+			return err
+		}
+
+		// add field
+		if err := collection.Fields.AddMarshaledJSONAt(11, []byte(`{
+			"hidden": false,
+			"id": "number1913416427",
+			"max": null,
+			"min": null,
+			"name": "current_enrollments",
+			"onlyInt": false,
+			"presentable": false,
+			"required": false,
+			"system": false,
+			"type": "number"
+		}`)); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	})
+}
